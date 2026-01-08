7e1c8ebb0d6a419bbc014f4adb98a80947c86f8e
s10313mp <s10313mp@ms.wwsi.edu.pl>
2026-01-08 02:03:51 +0100

    Add Counter component to App

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Moja Aplikacja React</h1>
+      <Counter />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
